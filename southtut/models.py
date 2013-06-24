86a0998607c25f6492f7841befe046f24518f864
Sirius Dely <siriusdely@icloud.com>
2013-06-24 13:37:39 +0700

    [+] Part 3 before southtut2

--- southtut/models.py
+++ southtut/models.py
@@ -5,3 +5,7 @@ class Knight (models.Model):
   of_the_round_table = models.BooleanField()
   dances_whenever_able = models.BooleanField()
   shrubberies = models.IntegerField(null=False)
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    facebook_page_id = models.CharField(max_length=255)
